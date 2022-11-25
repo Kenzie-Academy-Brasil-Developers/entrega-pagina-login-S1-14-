8a993b644e543f3e55105c5d9cf8ff73c7c1b267
pedrohenriquebarrosn <pedronascimentobr@hotmail.com>
2022-11-25 17:11:55 -0300

    envio

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,1 +1,8 @@
-/* Desenvolva seu código aqui... */
+const buttonTogleModal = document.querySelectorAll("[data-modal__control]")
+
+for (let i = 0; i < buttonTogleModal.length; i++) {
+    buttonTogleModal[i].addEventListener("click", function () {
+        let valueDataModalControl = buttonTogleModal[i].getAttribute("data-modal__control")
+        document.getElementById(valueDataModalControl).classList.toggle("show__modal")
+    })
+}
